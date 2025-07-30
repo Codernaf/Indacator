6de64456c05d8f29bfd2df74a51c667303534f17
Codernaf <abubarkarayonimisi@gmail.com>
2025-07-30 21:28:34 +0100

    Step 17: Add basic logger utility for tracking app activity

---
+++ utils/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+def setup_logger(name: str, log_file: str = 'app.log', level=logging.INFO):
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+    
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    
+    return logger
