0c926fc705e09b55751d52881d1b58804256f97c
Codernaf <abubarkarayonimisi@gmail.com>
2025-07-30 21:40:04 +0100

    Step 20: Add exchange configuration template for supported markets

---
+++ config/exchange_config.py
@@ -0,0 +1,22 @@
+EXCHANGES = {
+    'binance': {
+        'apiKey': 'your_binance_api_key',
+        'secret': 'your_binance_secret_key',
+        'enableRateLimit': True
+    },
+    'bybit': {
+        'apiKey': 'your_bybit_api_key',
+        'secret': 'your_bybit_secret_key',
+        'enableRateLimit': True
+    },
+    'bitget': {
+        'apiKey': 'your_bitget_api_key',
+        'secret': 'your_bitget_secret_key',
+        'enableRateLimit': True
+    },
+    'lbank': {
+        'apiKey': 'your_lbank_api_key',
+        'secret': 'your_lbank_secret_key',
+        'enableRateLimit': True
+    }
+}
